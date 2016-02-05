154a858e58673b5cdf16819c95d574f19df21297
Bryan MacDiarmid <bryanm@nulogy.com>
2016-02-05 13:08:35 -0500

    Updates gitignore

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2,
-    ENV['google_client_id'],
-    ENV['google_client_secret'],
-    hd: 'hashrocket.com',
-    access_type: 'online'
-end
