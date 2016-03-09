524b163be38840b7406bcacaceb69ce9f7986a6b
Ian Penney <ianp@nulogy.com>
2016-03-09 18:43:12 -0500

    New: PagesController to render letsencrypt challenge
    See http://collectiveidea.com/blog/archives/2016/01/12/lets-encrypt-with-a-rails-app-on-heroku
    https://trello.com/c/enK0lxaM

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  def letsencrypt
+    render text: "K5-FVom9Cay5uG-Dt2eNc-FRncVyKzTmaG26CBy51no.Ne1kldvrWwQwyKbGYZ6L7byDQxXhcjUyEB-CKGZyE-c"
+  end
+end
