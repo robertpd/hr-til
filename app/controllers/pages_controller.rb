088df22cdf31383ab5a28494e1fd2b4e2e846571
Clemens Park <clemensp@nulogy.com>
2016-06-09 16:19:23 -0400

    updates letsencrypt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "zIgJ1JIw7i0NhjGrapiK02a5Gkoi9pcs1uS8MiAU_LM.DRZ4iix3iEjfN65Nv7j15O_5a2enkcWmekj4YQ0K-wc"
+    render text: "CIyxIyja7_JfR7PUy28BqbSZEHl3CwsRj7eCraFSe4k.DRZ4iix3iEjfN65Nv7j15O_5a2enkcWmekj4YQ0K-wc"
   end
 end
