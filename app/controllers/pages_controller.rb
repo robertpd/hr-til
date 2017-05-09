8cbf061b90497f174e75602c46722450d599af9d
Bryan MacDiarmid <bryanmacdiarmid@bryan.local>
2017-05-09 13:08:32 -0400

    Updates letsencrypt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "ry74LES9pbImzdJjZCdC8uKnF-v_uu_gzqUyWppRXg4.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
+    render text: "fl4iuBKzUAWjbVRQ4q0A_pbkehie70a35udpd9OYId0.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
   end
 end
