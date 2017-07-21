f52c2d1ad0fd0475a0354d0cbffbc441d90002b3
Bryan MacDiarmid <bryanmacdiarmid@bryan.local>
2017-07-21 15:18:32 -0400

    Prompts for email

--- script/renew_cert.sh
+++ script/renew_cert.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-# brew install certbot
+read -p 'Email address to use for cert renewal: ' email
 
-sudo certbot certonly --staging --agree-tos --manual --non-interactive --logs-dir ../log/ --domain til-engineering.nulogy.com --email bryanm@nulogy.com --manual-public-ip-logging-ok --manual-auth-hook ./renew_cert_manual_auth_hook.sh --manual-cleanup-hook ./renew_cert_manual_cleanup_hook.sh
+brew install certbot
+
+sudo certbot certonly --staging --agree-tos --manual --non-interactive --logs-dir ../log/ --domain til-engineering.nulogy.com --email $email --manual-public-ip-logging-ok --manual-auth-hook ./renew_cert_manual_auth_hook.sh --manual-cleanup-hook ./renew_cert_manual_cleanup_hook.sh
