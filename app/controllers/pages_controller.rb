63ca8dc03e31867b62ddb7e564c8ab298355811e
System Administrator <root@bryan.local>
2017-07-21 16:38:05 -0400

    Updates letsencrypt challenge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "fl4iuBKzUAWjbVRQ4q0A_pbkehie70a35udpd9OYId0.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
+    render text: "${CERTBOT_VALIDATION}"
   end
 end
