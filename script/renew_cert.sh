ce7664e50d3b1300b4f9c076805b13505e98d86e
Bryan MacDiarmid <bryanm@nulogy.com>
2017-08-08 11:08:14 -0400

    Updates renewal scripts

--- script/renew_cert.sh
+++ script/renew_cert.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-#git checkout master
+git checkout master
 
 read -p 'Email address to use for cert renewal: ' email
 
 brew install certbot
 
-echo "When prompted below for password, enter your the password for your Nulogy account/Mac OS login."
+echo "If prompted below for password, enter your the password for your Nulogy account/Mac OS login."
 
-sudo certbot certonly --agree-tos --manual --non-interactive --logs-dir ./log/ --domain til-engineering.nulogy.com --email $email --manual-public-ip-logging-ok --manual-auth-hook ./script/renew_cert_manual_auth_hook.sh --manual-cleanup-hook ./script/renew_cert_manual_cleanup_hook.sh
+sudo certbot certonly --agree-tos --manual --manual-public-ip-logging-ok --manual-auth-hook ./script/renew_cert_manual_auth_hook.sh --manual-cleanup-hook ./script/renew_cert_manual_cleanup_hook.sh --non-interactive --logs-dir ./log/ --domain til-engineering.nulogy.com --email $email
