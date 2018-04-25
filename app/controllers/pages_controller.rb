e66f0b2fd9aa555804bf3808a0ec46738fc1ec92
Bryan MacDiarmid <bryanm@nulogy.com>
2018-04-25 15:08:30 -0400

    Updates letsencrypt challenge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "uX0ze8plML8zsnDxEGV6Z2HIa4g7a4KlEKdr0oo9WeI.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
+    render text: "_N1xsvl7Zjlftp-EC0diXTecm7Z3TaIin05tGPGK3EE.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
   end
 end
