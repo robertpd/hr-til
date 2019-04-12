b670ec002861667f163e51f3c98640c9dfc49244
Bryan MacDiarmid <bryanm@nulogy.com>
2019-04-12 10:34:31 -0400

    Update letsencrypt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "spe73Re32iP-0zctlSuQnh4QQl8qippcPkkV6dl8hfo.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
+    render text: "JHyxzM4cV4SqYp1gIEnpux6X4YnkbOYGqtuvKENujjE.roZBRoXEc6qJtoVbRBFghzcXYlT1sC6xOkeu19JJgkI"
   end
 end
